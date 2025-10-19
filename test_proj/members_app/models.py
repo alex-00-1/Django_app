70d6a61dd92a5f1e593f8bcdce436a3e0e197b98
oleksandr_lnx <zykoleksandr08@gmail.com>
2025-10-19 08:56:51 +0300

    change member model

--- test_proj/members_app/models.py
+++ test_proj/members_app/models.py
@@ -1,20 +1,19 @@
 from django.db import models
 
 
-# Константа, яка створюєтья напряму через СУБД. Допустимі значення -> ["Male", "Female", "Hidden"]
-class MemberSex(models.Model):
-    sex = models.CharField()
-
-    def __str__(self):
-        return self.sex
-
 
 class MembersModel(models.Model):
+    geneders = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('H', 'Hidden'),
+    ]
+
     firstname = models.CharField(max_length=25)
     lastname = models.CharField(max_length=25)
     age = models.IntegerField()
     email = models.EmailField(unique=True, null=True)
-    sex = models.ForeignKey(MemberSex, on_delete=models.SET_NULL , null=True, default="Hidden")
+    sex = models.CharField(choices=geneders, default="H", null=True)
 
 
     def __str__(self):
